e5c28d4fd0a801277f4f51abc3dce128207e1b74
julien pierlot <j.pierlot@thesocialclient.com>
2019-09-25 23:57:54 +0200

    added random method on event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
   has_many :participants, dependent: :destroy
   validates :name, :budget, :location, :date, presence: true
+
+  def randomize(participants)
+    result = {}
+    participants.each do |giver|
+      receiver = participants.select { |receiver| receiver != giver }
+      result[giver] = receiver.sample
+    end
+  end
+
+
 end

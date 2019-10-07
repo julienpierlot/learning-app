542eb8f9f8474fd786dd3202093eabf04cfd3e55
julien pierlot <j.pierlot@thesocialclient.com>
2019-10-08 01:10:51 +0200

    deleted unused method

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,14 +2,6 @@ class Event < ApplicationRecord
   has_many :participants, dependent: :destroy
   validates :name, :budget, :location, :date, presence: true
 
-  def randomize(participants)
-    result = {}
-    participants.each do |giver|
-      receiver = participants.select { |receiver| receiver != giver }
-      result[giver] = receiver.sample
-    end
-  end
-
   def add_participant(emails)
     emails_already_assigned = []
     emails.each do |email|

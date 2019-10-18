0853df6a5738dde79ebf542ece9557053fb56035
Julien Pierlot <julien.pierlot@tsc.digital>
2019-10-18 15:18:09 +0200

    created method to generate unique token in participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,14 @@
 class Participant < ApplicationRecord
   belongs_to :event
-  validates :email, presence: true
+  validates :email, :receiver, presence: true
+
+  def generate_token
+   begin
+    self.email_token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless self.class.exists?(email_token: token)
+    end
+   end
+  end
 
 end

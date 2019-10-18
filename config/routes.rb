1fb5b8e5f00d6da39551356dcb4849c16608f1da
Julien Pierlot <julien.pierlot@tsc.digital>
2019-10-18 15:18:44 +0200

    added route to participant show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   resources :articles do
     resources :comments
   end
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :events
+
+  resources :participants, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

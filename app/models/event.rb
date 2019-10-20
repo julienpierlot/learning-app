7179cd12e71438fb6c54d87ff6c4508e6230c484
julien pierlot <j.pierlot@thesocialclient.com>
2019-10-20 22:54:27 +0200

    generate random token for participant

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
     emails.each do |email|
       receiver = (emails - emails_already_assigned).select { |receiver| receiver != email }.sample
       emails_already_assigned << receiver
-      self.participants << Participant.new(email: email, receiver: receiver)
+      self.participants << Participant.new(email: email, receiver: receiver, email_token: SecureRandom.urlsafe_base64(nil, false))
     end
   end
 

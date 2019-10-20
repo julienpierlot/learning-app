02fcb052cf3ae3d845a90a63baf130c41465b1bc
julien pierlot <j.pierlot@thesocialclient.com>
2019-10-20 22:36:57 +0200

    authorizing participant to see only his page

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,6 +2,10 @@ class ParticipantsController < ApplicationController
 
   def show
     @participant = Participant.find(params[:id])
+    unless (user_signed_in? && (current_user.id == @participant.event.user_id)) && (params["token"].present? && params["token"] == @participant.email_token)
+    redirect_to root_path
+    flash[:notice] = "Not authorized to access this page"
+    end
   end
 
 end

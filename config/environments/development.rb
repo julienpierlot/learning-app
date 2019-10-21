98b7c6ee5e51d8899920a58a1f55375a65e7dc26
Julien Pierlot <julien.pierlot@tsc.digital>
2019-10-21 11:16:19 +0200

    Commented mail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,13 +10,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => "pierlot.julien",
-   :password             => "nHLE5tD8,;",
-   :authentication       => "plain",
-   :enable_starttls_auto => true
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "com",
+  #  :port                 => 587,
+  #  :user_name            => "",
+  #  :password             => "",
+  #  :authentication       => "plain",
+  #  :enable_starttls_auto => true
 
   config.cache_classes = false
 

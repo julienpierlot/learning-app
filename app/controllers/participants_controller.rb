6630bcdba190b83cca59a071b4d6dce5742bc51c
julien pierlot <j.pierlot@thesocialclient.com>
2019-10-24 22:17:22 +0200

    fix condition on participant show

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,7 +2,7 @@ class ParticipantsController < ApplicationController
 
   def show
     @participant = Participant.find(params[:id])
-    unless (user_signed_in? && (current_user.id == @participant.event.user_id)) && (params["token"].present? && params["token"] == @participant.email_token)
+    unless (user_signed_in? && (current_user.id == @participant.event.user_id)) || (params["token"].present? && params["token"] == @participant.email_token)
     redirect_to root_path
     flash[:notice] = "Not authorized to access this page"
     end

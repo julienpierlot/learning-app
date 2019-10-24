7eabc3cebb48ac90a0fa51992054a2164aa44542
julien pierlot <j.pierlot@thesocialclient.com>
2019-10-24 21:48:52 +0200

    config email production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'pierlot.julien@gmail.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'jpgooner',
-  :password => 'nHLE5tD8,;',
-  :domain => 'learning-app-ju.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
